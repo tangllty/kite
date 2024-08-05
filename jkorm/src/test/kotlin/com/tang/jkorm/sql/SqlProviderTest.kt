ebaaf7a7be7a645aa1c711f63dd0ac374f6ffb0e
Tang <tangllty@163.com>
2024-08-05 13:59:49 +0800

    Test delete and select sql provider

--- jkorm/src/test/kotlin/com/tang/jkorm/sql/SqlProviderTest.kt
+++ jkorm/src/test/kotlin/com/tang/jkorm/sql/SqlProviderTest.kt
@@ -15,28 +15,48 @@ class SqlProviderTest {
     fun insert() {
         val account = Account(username = "tang", password = "123456")
         val sql = SqlProvider.insert(account)
-        assertEquals("insert into account (id,username,password) values (null,'tang','123456')", sql.lowercase())
+        assertEquals("insert into account (id,username,password) values (null,'tang','123456')", sql)
     }
 
     @Test
     fun insertSelective() {
         val account = Account(username = "tang", password = "123456")
         val sql = SqlProvider.insertSelective(account)
-        assertEquals("insert into account (username,password) values ('tang','123456')", sql.lowercase())
+        assertEquals("insert into account (username,password) values ('tang','123456')", sql)
     }
 
     @Test
     fun update() {
         val account = Account(id = 1, username = "tang", password = "123456")
         val sql = SqlProvider.update(account)
-        assertEquals("update account set username='tang',password='123456' where id = 1", sql.lowercase())
+        assertEquals("update account set username='tang',password='123456' where id = 1", sql)
     }
 
     @Test
     fun updateSelective() {
         val account = Account(id = 1, password = "123456")
         val sql = SqlProvider.updateSelective(account)
-        assertEquals("update account set password='123456' where id = 1", sql.lowercase())
+        assertEquals("update account set password='123456' where id = 1", sql)
+    }
+
+    @Test
+    fun delete() {
+        val account = Account(id = 1)
+        val sql = SqlProvider.delete(Account::class.java, account)
+        assertEquals("delete from account where id = 1", sql)
+    }
+
+    @Test
+    fun select() {
+        val sql = SqlProvider.select(Account::class.java, null)
+        assertEquals("select * from account", sql)
+    }
+
+    @Test
+    fun selectCondition() {
+        val account = Account(username = "tang")
+        val sql = SqlProvider.select(Account::class.java, account)
+        assertEquals("select * from account where username='tang'", sql)
     }
 
 }
