df58fccd6d55cc01afc8efea50b3577a81c7e230
Tang <tangllty@163.com>
2024-08-07 10:52:29 +0800

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,5 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
--- jkorm/build.gradle.kts
+++ jkorm/build.gradle.kts
@@ -12,15 +12,15 @@ dependencies {
     // https://mvnrepository.com/artifact/org.yaml/snakeyaml
     implementation("org.yaml:snakeyaml:2.2")
     // https://mvnrepository.com/artifact/org.apache.derby/derby
-    implementation("org.apache.derby:derby:10.14.2.0")
+    implementation("org.apache.derby:derby:10.16.1.1")
     // https://mvnrepository.com/artifact/org.apache.derby/derbytools
-    implementation("org.apache.derby:derbytools:10.14.2.0")
+    implementation("org.apache.derby:derbytools:10.16.1.1")
     // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
     implementation("com.mysql:mysql-connector-j:8.3.0")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.13")
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-    testImplementation("ch.qos.logback:logback-classic:1.3.14")
+    testImplementation("ch.qos.logback:logback-classic:1.5.6")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
@@ -30,5 +30,5 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
