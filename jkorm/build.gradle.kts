ad9d881353af51b3d9378e23a1c613e1f374bbc2
Tang <tangllty@163.com>
2024-08-08 16:49:45 +0800

    Add libs.versions.toml

--- jkorm/build.gradle.kts
+++ jkorm/build.gradle.kts
@@ -9,19 +9,11 @@ repositories {
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/org.yaml/snakeyaml
-    implementation("org.yaml:snakeyaml:2.2")
-    // https://mvnrepository.com/artifact/org.apache.derby/derby
-    implementation("org.apache.derby:derby:10.16.1.1")
-    // https://mvnrepository.com/artifact/org.apache.derby/derbytools
-    implementation("org.apache.derby:derbytools:10.16.1.1")
-    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
-    implementation("com.mysql:mysql-connector-j:8.3.0")
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-    implementation("org.slf4j:slf4j-api:2.0.13")
-    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-    testImplementation("ch.qos.logback:logback-classic:1.5.6")
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
+    implementation(libs.snakeyaml)
+    implementation(libs.derby)
+    implementation(libs.derbytools)
+    implementation(libs.slf4j)
+    testImplementation(libs.logback)
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
 }
