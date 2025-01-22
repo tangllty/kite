07807256e0b65eda3211f2aac1d06a436b71c0f8
Tang <tangllty@163.com>
2025-01-22 10:38:06 +0800

    Optimize spring transaction

--- jkorm-spring/src/main/kotlin/com/tang/jkorm/spring/proxy/SqlSessionProxy.kt
+++ jkorm-spring/src/main/kotlin/com/tang/jkorm/spring/proxy/SqlSessionProxy.kt
@@ -13,6 +13,9 @@ import java.lang.reflect.Method
 class SqlSessionProxy(private val sqlSessionFactory: SqlSessionFactory) : InvocationHandler {
 
     override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any {
+        if (method.name != "execute") {
+            return method.invoke(sqlSessionFactory, *(args ?: emptyArray()))
+        }
         val sqlSession = SqlSessions.openSession(sqlSessionFactory)
         val result = method.invoke(sqlSession, *(args ?: emptyArray()))
         sqlSession.commit()
--- jkorm-spring/src/main/kotlin/com/tang/jkorm/spring/utils/SqlSessions.kt
+++ jkorm-spring/src/main/kotlin/com/tang/jkorm/spring/utils/SqlSessions.kt
@@ -4,6 +4,7 @@ import com.tang.jkorm.logging.LOGGER
 import com.tang.jkorm.session.SqlSession
 import com.tang.jkorm.session.factory.SqlSessionFactory
 import com.tang.jkorm.spring.holder.SqlSessionHolder
+import com.tang.jkorm.spring.transaction.SpringTransactionSynchronization
 import org.springframework.transaction.support.TransactionSynchronizationManager
 
 /**
@@ -13,23 +14,40 @@ object SqlSessions {
 
     fun openSession(sqlSessionFactory: SqlSessionFactory): SqlSession {
         if (TransactionSynchronizationManager.hasResource(sqlSessionFactory)) {
-            LOGGER.info("Get the sql session from the transaction synchronization manager")
-            return (TransactionSynchronizationManager.getResource(sqlSessionFactory) as SqlSessionHolder).sqlSession
+            val holder = TransactionSynchronizationManager.getResource(sqlSessionFactory) as SqlSessionHolder
+            if (holder.isSynchronizedWithTransaction) {
+                holder.requested()
+                LOGGER.debug("Get the SqlSession[{}] from the transaction synchronization manager", holder.sqlSession)
+                return holder.sqlSession
+            }
         }
+        LOGGER.debug("Open a new sql session")
         val session = sqlSessionFactory.openSession()
-        TransactionSynchronizationManager.bindResource(sqlSessionFactory, SqlSessionHolder(session))
-        LOGGER.info("Open a new sql session")
+        registerTransactionSynchronization(sqlSessionFactory, SqlSessionHolder(session))
         return session
     }
 
     fun closeSession(sqlSessionFactory: SqlSessionFactory, sqlSession: SqlSession) {
         val holder = TransactionSynchronizationManager.getResource(sqlSessionFactory) as SqlSessionHolder?
         if (holder != null && holder.sqlSession == sqlSession) {
+            LOGGER.debug("Releasing transaction synchronization for SqlSession[{}]", holder.sqlSession)
             holder.released()
         } else {
-            LOGGER.info("Close the sql session")
+            LOGGER.debug("Close the sql session")
             sqlSession.close()
         }
+    }
+
+    private fun registerTransactionSynchronization(sqlSessionFactory: SqlSessionFactory, holder: SqlSessionHolder) {
+        if (TransactionSynchronizationManager.isSynchronizationActive().not()) {
+            return
+        }
+        LOGGER.debug("Registering transaction synchronization for SqlSession[{}]", holder.sqlSession)
+        holder.requested()
+        TransactionSynchronizationManager.bindResource(sqlSessionFactory, holder)
+        TransactionSynchronizationManager.registerSynchronization(SpringTransactionSynchronization(holder, sqlSessionFactory))
+        holder.isSynchronizedWithTransaction = true
+        holder.requested()
     }
 
 }
