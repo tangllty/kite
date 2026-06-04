f995769767f36bd994542022e8222f6165d1fe7c
Tang <tangllty@163.com>
2026-06-04 15:43:52 +0800

    Add documentation comments to `PropertyApplier`

--- kite-spring-boot-autoconfigure/src/main/kotlin/com/tang/kite/boot/autoconfigure/PropertyApplier.kt
+++ kite-spring-boot-autoconfigure/src/main/kotlin/com/tang/kite/boot/autoconfigure/PropertyApplier.kt
@@ -1,6 +1,8 @@
 package com.tang.kite.boot.autoconfigure
 
 /**
+ * Interface for applying configuration properties to the global Kite configuration objects.
+ *
  * @author Tang
  */
 interface PropertyApplier {
